fcbe7f297427b932bfab657e9a6907771f46f91a
Danielx31 <102207384+Danielx31@users.noreply.github.com>
2026-02-24 15:31:01 +0800

    feat(tasks): dispatch TaskCreated event on task creation

--- app/Services/TaskServices.php
+++ app/Services/TaskServices.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Events\TaskCreated;
 use App\Exceptions\TaskAlreadyCompletedException;
 use App\Models\Task;
 use App\Services\Contracts\TaskServiceInterface;
@@ -10,7 +11,12 @@ class TaskServices implements TaskServiceInterface
 {
     public function create(array $data): Task
     {
-        return Task::create($data);
+
+        $task = Task::create($data);
+
+        event(new TaskCreated($task));
+
+        return $task;
     }
 
     public function completed(Task $task): Task
